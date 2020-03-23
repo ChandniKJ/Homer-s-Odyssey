30a4e982abf5c92d5069ba95f03ba2bba99773af
ChandniKJ <54037953+ChandniKJ@users.noreply.github.com>
2020-03-23 11:16:37 +0000

    Create quest010

---
+++ front/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import SignUp from "../src/components/SignUp";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
