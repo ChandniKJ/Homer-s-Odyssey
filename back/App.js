75db99e9b99230163bb396a718601d38a4e0df0b
ChandniKJ <54037953+ChandniKJ@users.noreply.github.com>
2020-03-30 16:56:22 +0100

    Create quest040

--- back/App.js
+++ back/App.js
@@ -5,20 +5,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const authRouter = require("./routes/auth/Auth");
-const app = express();
 
-app.use("/auth", authRouter); //where authRouter is imported
+const app = express();
 
 // set up the application
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
+app.use("/auth", authRouter); //where authRouter is imported
 
 // implement the API part
 app.get("/", (req, res) => {
   res.send("youhou");
 });
+
 /// in case path is not found, return the 'Not Found' 404 code
 app.use(function(req, res, next) {
   var err = new Error("Not Found");
